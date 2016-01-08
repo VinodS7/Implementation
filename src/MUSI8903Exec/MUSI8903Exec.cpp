cfbac6187f0d7a88edb6ed62429399cf4a14b576
lerch <lerch@zplane.de>
2016-01-08 13:13:33 -0500

    - generate a project for a command line target with empty main function with CMake

---
+++ src/MUSI8903Exec/MUSI8903Exec.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+
+// local function declarations
+void    showClInfo ();
+
+/////////////////////////////////////////////////////////////////////////////////
+// main function
+int main(int argc, char* argv[])
+{
+    std::string             sInputFilePath,                 //!< file paths
+                            sOutputFilePath;
+
+    long long               iInFileLength       = 0;        //!< length of input file
+
+    clock_t                 time                = 0;
+
+    float                   **ppfAudioData      = 0;
+
+    showClInfo ();
+
+    //////////////////////////////////////////////////////////////////////////////
+    // parse command line arguments
+
+    //////////////////////////////////////////////////////////////////////////////
+    // open the input wave file
+
+    //////////////////////////////////////////////////////////////////////////////
+    // allocate memory
+
+    // get audio data
+
+    //////////////////////////////////////////////////////////////////////////////
+    // get audio info and print it to stdout
+
+    //////////////////////////////////////////////////////////////////////////////
+    // do processing
+    cout << "Hello there!" << endl << endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // clean-up
+
+    return 0;
+    
+}
+
+
+void     showClInfo()
+{
+    cout << "GTCMT MUSI8903" << endl;
+    cout << "(c) 2016 by Alexander Lerch" << endl;
+    cout  << endl;
+
+    return;
+}
+
